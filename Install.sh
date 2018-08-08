e555760b7769ec90b51c05800fca940f422c0116
NitramLegov <Martin.Vogel@live.com>
2018-08-08 02:31:24 +0200

    .

--- Install.sh
+++ Install.sh
@@ -1,6 +1,8 @@
 echo 'Starting with the Musicbox installation'
 sudo ./Install_SystemSettings.sh
+sudo ./Install_Shutdown_LED.sh
 sudo ./Install_BT_Speaker.sh
+
 
 #temporarily disabled
 sudo ./Install_Mopidy.sh
